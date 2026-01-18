cbc6bbaea394d35f2964c067ed571c99ead84bd8
Serdar <serdar@live.fi>
2026-01-18 17:35:31 +0200

    Remove unnecessary break statement in transcription loop
    Signed-off-by: Serdar <serdar@live.fi>

--- app/application/use_cases/transcribe_audio_use_case.py
+++ app/application/use_cases/transcribe_audio_use_case.py
@@ -44,7 +44,7 @@ class TranscribeAudioUseCase:
                 whisper_hailo.send_data(mel)
                 time.sleep(0.2)
                 result += clean_transcription(whisper_hailo.get_transcription())
-                break
+                
 
             return result
         else:
